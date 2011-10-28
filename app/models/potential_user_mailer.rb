0def8b79bad9ccf71da2e1881b56919a098d8d5c
Todd Persen <todd.persen@gmail.com>
2011-10-28 11:05:53 -0400

    BCC notifications@cozi.ly in addition to the finder/lister.

--- app/models/potential_user_mailer.rb
+++ app/models/potential_user_mailer.rb
@@ -1,12 +1,14 @@
 class PotentialUserMailer < ActionMailer::Base
   def potential_lister_message(email)
     from "support@cozi.ly"
+    bcc "notifications@cozi.ly"
     recipients email
     subject "List your apartment on Cozily"
   end
 
    def potential_finder_message(email)
     from "support@cozi.ly"
+    bcc "notifications@cozi.ly"
     recipients email
     subject "Find your apartment on Cozily"
   end
